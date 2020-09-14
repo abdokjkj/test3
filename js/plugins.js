6ae94cccbc101d08724329f30ebd7b97b7606947
abdokjkj <71026403+abdokjkj@users.noreply.github.com>
2020-09-14 14:10:14 +0200

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function(){
+
+"use strict";
+
+document.getElementById("fixed").style.backgroundColor = "aqua";
+document.getElementById("fixed").style.width = "100%";
+
+
+
+}
+
+
